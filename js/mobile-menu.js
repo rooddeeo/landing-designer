1adde7ccfd3b84eaed98c5418b34d3d96a61c44b
rooddeeo <rooddeeo@gmail.com>
2024-06-11 19:31:37 +0300

    Fix styles

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,6 +1,6 @@
 function toggleMenu() {
-  var menu = document.querySelector(".header-mob-menu");
-  var backdrop = document.querySelector(".backdrop");
+  const menu = document.querySelector(".header-mob-menu");
+  const backdrop = document.querySelector(".backdrop");
   menu.classList.toggle("open");
   backdrop.classList.toggle("backdropOn");
 }
